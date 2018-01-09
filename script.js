2195b052eb0a7b054b2c247e0073d4af05cd19c8
kes2401 <keithscully83@gmail.com>
2018-01-09 22:16:15 +0000

    add player shots to player shots array, add comments

--- script.js
+++ script.js
@@ -12,15 +12,28 @@ var winCombos = [
 
 var playerShots = []; // this will hold an array of all shots played by the user
 var cpuShots = []; // this will hold an array of all shots played by the computer
+var currentShot;
+
+var playerTurn = true; // controls whether it is the player's turn or computer's turn
 
 
-
-
-
-// event handler to place mark on selected cell
+// event handler to place mark on selected cell, add cell to shot array, and check whether there is a winner
 $('.cell').on('click', function(e){
     (this).innerHTML = 'X';
 
     // get id of the cell clicked and pass it to an array of shots played 
+    currentShot = $(this).attr('id');
+    playerShots.push(currentShot);
+    playerShots.sort() // -----does it need to be sorted??? review when coding for comparison check with win combos
+    console.log('current shot: ' + currentShot);
+    console.log('player shots array: ' + playerShots);
+
+    // compare shots array to winning combos
+
+
+    // if winner end game and show win modal
+
+
+    // finally, if not a winner change control to cpu player
 
 });
